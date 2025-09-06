09cf537659414ee5be67bfbe4406fec30724a012
Kayson Summers <slsummers28@outlook.com>
2025-09-05 22:16:57 -0600

    Added site settings

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+function Light(){
+    document.getElementById("mainpage").style="background-color:white; color: black;";
+    document.getElementById("mode").innerHTML="Light";
+} 
+
+function Dark(){
+    document.getElementById("mainpage").style="background-color:black; color: white;";
+    document.getElementById("mode").innerHTML="Dark";
+} 
+
+function Default(){
+    document.getElementById("mainpage").style="background-color:#dea561; color: bisque;";
+    document.getElementById("mode").innerHTML="Default";
+} 
